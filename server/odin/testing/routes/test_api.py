f7973344bf986ff458c2a92a3de5517a8b2ec132
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2016-04-19 19:44:04 +0100

    Fixed py3 issue of mocking RequestHandler
    Removed mocking of __bases__ of ApiHandler, create mock
    application and request objects instead. Not as neat, but it
    works at least!

--- server/odin/testing/routes/test_api.py
+++ server/odin/testing/routes/test_api.py
@@ -1,14 +1,12 @@
 import sys
 import json
 
-import tornado.web
-
 from nose.tools import *
 
 if sys.version_info[0] == 3:
-    from unittest.mock import Mock, patch
+    from unittest.mock import Mock
 else:
-    from mock import Mock, patch
+    from mock import Mock
 
 from odin.http.routes.api import ApiRoute, ApiHandler, ApiError, _api_version
 
@@ -52,85 +50,92 @@ class TestApiHandler():
         # Initialise attribute to receive output of patched write() method
         cls.write_data = None
 
-        # Patch the base class of ApiHandler, i.e. replace tornado.web.RequestHandler,
-        # which allows testing to take place without the need to instantiate a full
-        # tornado web application
-        print(ApiHandler.__bases__)
-        cls.patcher = patch.object(ApiHandler, '__bases__', (Mock,))
-        cls.mock_request_handler = cls.patcher.start()
-        print("PATCHER STARTED")
-        print(ApiHandler.__bases__)
+        # Create mock Tornado application and request objects for ApiHandler initialisation
+        cls.app = Mock()
+        cls.app.ui_methods = {}
+        cls.request = Mock()
 
-        cls.patcher.is_local = True
+        # Create mock responses for ApiHandler test cases
+        cls.json_dict_response = Mock()
+        cls.json_dict_response.status_code = 200
+        cls.json_dict_response.content_type = 'application/json'
+        cls.json_dict_response.data = {'response': 'is_json'}
 
-        # # Create the patched handler and mock its write method with the local version
-        # cls.handler = ApiHandler()
-        # cls.handler.write = cls.mock_write
-        #
-        # cls.json_dict_response = Mock()
-        # cls.json_dict_response.status_code = 200
-        # cls.json_dict_response.content_type = 'application/json'
-        # cls.json_dict_response.data = {'response': 'is_json'}
-        #
-        # cls.json_str_response = Mock()
-        # cls.json_str_response.status_code = 200
-        # cls.json_str_response.content_type = 'application/json'
-        # cls.json_str_response.data = json.dumps(cls.json_dict_response.data)
-        #
-        # # Create a mock route and a default adapter for a subsystem
-        # cls.route = Mock()
-        # cls.subsystem = 'default'
-        # cls.route.adapters = {}
-        # cls.route.adapter = lambda subsystem: cls.route.adapters[subsystem]
-        #
-        # cls.route.adapters[cls.subsystem] = Mock()
-        # cls.route.adapters[cls.subsystem].get.return_value = cls.json_dict_response
-        # cls.route.adapters[cls.subsystem].put.return_value = cls.json_dict_response
-        # cls.route.adapters[cls.subsystem].delete.return_value = cls.json_dict_response
-        # cls.handler.initialize(cls.route)
+        cls.json_str_response = Mock()
+        cls.json_str_response.status_code = 200
+        cls.json_str_response.content_type = 'application/json'
+        cls.json_str_response.data = json.dumps(cls.json_dict_response.data)
 
-    @classmethod
-    def teardown_class(cls):
-        print("PATCHER STOPPED")
-        print(ApiHandler.__bases__)
+        # Create a mock route and a default adapter for a subsystem
+        cls.route = Mock()
+        cls.subsystem = 'default'
+        cls.route.adapters = {}
+        cls.route.adapter = lambda subsystem: cls.route.adapters[subsystem]
+        cls.route.has_adapter = lambda subsystem: subsystem in cls.route.adapters
 
-        cls.patcher.stop()
-        print(ApiHandler.__bases__)
-        cls.patcher = patch.object(ApiHandler, '__bases__', (tornado.web.RequestHandler))
+        cls.route.adapters[cls.subsystem] = Mock()
+        cls.route.adapters[cls.subsystem].get.return_value = cls.json_dict_response
+        cls.route.adapters[cls.subsystem].put.return_value = cls.json_dict_response
+        cls.route.adapters[cls.subsystem].delete.return_value = cls.json_dict_response
 
-        print(ApiHandler.__bases__)
+        # Create the handler and mock its write method with the local version
+        cls.handler = ApiHandler(cls.app, cls.request, route=cls.route)
+        cls.handler.write = cls.mock_write
 
-    def test_nothing(self):
+        cls.path = 'default/path'
 
-        assert_equal(1, 1)
+    def test_handler_valid_get(self):
 
-    # def test_handler_valid_get(self):
-    #
-    #     path = '/handler/test'
-    #
-    #     self.handler.get(str(_api_version), self.subsystem, path)
-    #     assert_equal(json.loads(self.write_data), self.json_dict_response.data)
-    #
-    # def test_handler_response_json_str(self):
-    #
-    #     self.handler.respond(self.json_str_response)
-    #     assert_equal(self.write_data, self.json_str_response.data)
-    #
-    # def test_handler_response_json_dict(self):
-    #
-    #     self.handler.respond(self.json_dict_response)
-    #     assert_equal(self.write_data, self.json_str_response.data)
-    #
-    # def test_handler_response_json_bad_type(self):
-    #
-    #     bad_response = Mock()
-    #     bad_response.status_code = 200
-    #     bad_response.content_type = 'application/json'
-    #     bad_response.data = 1234
-    #
-    #     with assert_raises_regexp(ApiError,
-    #           'A response with content type application/json must have str or dict data'):
-    #         self.handler.respond(bad_response)
+        self.handler.get(str(_api_version), self.subsystem, self.path)
+        assert_equal(self.handler.get_status(), 200)
+        assert_equal(json.loads(self.write_data), self.json_dict_response.data)
+
+    def test_handler_valid_put(self):
+
+        self.handler.put(str(_api_version), self.subsystem, self.path)
+        assert_equal(self.handler.get_status(), 200)
+        assert_equal(json.loads(self.write_data), self.json_dict_response.data)
+
+    def test_handler_valid_delete(self):
+
+        self.handler.delete(str(_api_version), self.subsystem, self.path)
+        assert_equal(self.handler.get_status(), 200)
+        assert_equal(json.loads(self.write_data), self.json_dict_response.data)
+
+    def test_bad_api_version(self):
+
+        bad_version = 0.1234
+        self.handler.get(str(bad_version), self.subsystem, self.path)
+        assert_equal(self.handler.get_status(), 400)
+        assert_equal(self.write_data, "API version {} is not supported".format(bad_version))
+
+    def test_bad_subsystem(self):
+
+        bad_subsystem = 'missing'
+        self.handler.get(str(_api_version), bad_subsystem, self.path)
+        assert_equal(self.handler.get_status(), 400)
+        assert_equal(self.write_data, "No API adapter registered for subsystem {}".format(bad_subsystem))
+
+    def test_handler_response_json_str(self):
+
+        self.handler.respond(self.json_str_response)
+        assert_equal(self.write_data, self.json_str_response.data)
+
+    def test_handler_response_json_dict(self):
+
+        self.handler.respond(self.json_dict_response)
+        assert_equal(self.write_data, self.json_str_response.data)
+
+    def test_handler_response_json_bad_type(self):
+
+        bad_response = Mock()
+        bad_response.status_code = 200
+        bad_response.content_type = 'application/json'
+        bad_response.data = 1234
+
+        with assert_raises_regexp(ApiError,
+              'A response with content type application/json must have str or dict data'):
+            self.handler.respond(bad_response)
 
 
 
