079ff80854a7a462b163b7db5c47b7ab65f6efaf
Tim Nicholls <timcnicholls@gmail.com>
2016-05-11 20:07:59 +0100

    Completing docstrings for api.py

--- server/odin/http/routes/api.py
+++ server/odin/http/routes/api.py
@@ -1,3 +1,11 @@
+"""API-related classes for the ODIN server.
+
+This module implements a number of classes used by the ODIN server for routing and handling
+API calls.
+
+Tim Nicholls, STFC Application Engineering Group
+"""
+
 import importlib
 import logging
 import json
@@ -11,9 +19,10 @@ _api_version = 0.1
 
 
 def validate_api_request(required_version):
-    """
-    Checks API version is correct and that the subsystem is registered with the
-    application dispatcher; responds with a 400 error if not
+    """Validate an API request to the ApiHandler.
+
+    This decorator checks that API version in the URI of a requst is correct and that the subsystem
+    is registered with the application dispatcher; responds with a 400 error if not
     """
     def decorator(func):
         def wrapper(_self, *args, **kwargs):
@@ -38,44 +47,84 @@ def validate_api_request(required_version):
 
 
 class ApiError(Exception):
+    """Simple exception class for API-related errors."""
+
     pass
 
 
 class ApiVersionHandler(tornado.web.RequestHandler):
+    """API version handler to allow client to resolve supported version.
+
+    This request handler implements the GET verb to allow a call to the appropriate URI to return
+    the supported API version as JSON.
+    """
 
     def get(self):
+        """Handle API version GET requests."""
         if 'Accept' in self.request.headers:
             accept_type = self.request.headers['Accept']
             if accept_type == "*/*" or accept_type == 'application/json':
                 self.set_status(200)
-                self.write(json.dumps({'api_version' : _api_version}))
+                self.write(json.dumps({'api_version': _api_version}))
             else:
                 self.set_status(406)
                 self.write('Requested content types not supported')
 
 
 class ApiHandler(tornado.web.RequestHandler):
+    """API handler to transform requests into appropriate adapter calls.
+
+    This handler maps incoming API requests into the appropriate calls to methods
+    in registered adapters. HTTP GET, PUT and DELETE verbs are supported. The class
+    also enforces a uniform response with the appropriate Content-Type header.
+    """
 
     def initialize(self, route):
+        """Initialize the API handler.
+
+        :param route: ApiRoute object calling the handler (allows adapters to be resolved)
+        """
         self.route = route
 
     @validate_api_request(_api_version)
     def get(self, subsystem, path):
+        """Handle an API GET request.
+
+        :param subsystem: subsystem element of URI, defining adapter to be called
+        :param path: remaining URI path to be passed to adapter method
+        """
         response = self.route.adapter(subsystem).get(path, self.request)
         self.respond(response)
 
     @validate_api_request(_api_version)
     def put(self, subsystem, path):
+        """Handle an API PUT request.
+
+        :param subsystem: subsystem element of URI, defining adapter to be called
+        :param path: remaining URI path to be passed to adapter method
+        """
         response = self.route.adapter(subsystem).put(path, self.request)
         self.respond(response)
 
     @validate_api_request(_api_version)
     def delete(self, subsystem, path):
+        """Handle an API DELETE request.
+
+        :param subsystem: subsystem element of URI, defining adapter to be called
+        :param path: remaining URI path to be passed to adapter method
+        """
         response = self.route.adapter(subsystem).delete(path, self.request)
         self.respond(response)
 
     def respond(self, response):
+        """Respond to an API request.
 
+        This method transforms an ApiAdapterResponse object into the appropriate request handler
+        response, setting the HTTP status code and content type for a response to an API request
+        and validating the content of the response against the appropriate type.
+
+        :param response: ApiAdapterResponse object containing response
+        """
         self.set_status(response.status_code)
         self.set_header('Content-Type', response.content_type)
 
@@ -91,9 +140,10 @@ class ApiHandler(tornado.web.RequestHandler):
 
 
 class ApiRoute(Route):
+    """ApiRoute - API route object used to map handlers onto adapter for API calls."""
 
     def __init__(self):
-
+        """Initialize the ApiRoute object."""
         super(ApiRoute, self).__init__()
 
         # Define a default handler which can return the support API version
@@ -109,9 +159,7 @@ class ApiRoute(Route):
         self.adapters = {}
 
     def register_adapter(self, adapter_config, fail_ok=True):
-
-        """
-        Registers an API adapter with the APIRoute object.
+        """Register an API adapter with the APIRoute object.
 
         Based on the adapter_config object passed in as an argument, this method attempts to
         load the specified module and create an instance of the adapter class to be used
@@ -120,7 +168,6 @@ class ApiRoute(Route):
         :param adapter_config: AdapterConfig object for the adapter
         :param fail_ok: Allow the adapter import and registration to fail without raising an error
         """
-
         # Resolve the adapter module and class name from the dotted module path in the config object
         (module_name, class_name) = adapter_config.module.rsplit('.', 1)
 
@@ -143,9 +190,17 @@ class ApiRoute(Route):
             )
 
     def has_adapter(self, subsystem):
+        """Determine if ApiRoute object has adapter for subsystem.
 
+        :param subsystem: subsystem to check for adapter
+        :return: True of adapter present for subsystem
+        """
         return subsystem in self.adapters
 
     def adapter(self, subsystem):
+        """Return adapter for subsystem.
 
+        :param subsystem: subsystem to return adapter for
+        :return: adapter for subsystem
+        """
         return self.adapters[subsystem]
