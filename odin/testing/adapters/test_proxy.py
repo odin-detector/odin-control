4ce7e150b5c9e7d08bda5328c8f43fa65f60d422
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2018-07-10 11:50:38 +0100

    Added ProxyAdapter test cases

--- odin/testing/adapters/test_proxy.py
+++ odin/testing/adapters/test_proxy.py
@@ -1,4 +1,6 @@
 import sys
+import threading
+import logging
 
 if sys.version_info[0] == 3:  # pragma: no cover
     from unittest.mock import Mock, patch
@@ -7,8 +9,6 @@ else:                         # pragma: no cover
 
 from nose.tools import *
 
-import threading
-
 from tornado.testing import AsyncHTTPTestCase, bind_unused_port
 from tornado.ioloop import IOLoop
 from tornado.web import Application, RequestHandler
@@ -16,6 +16,7 @@ from tornado.httpserver import HTTPServer
 import tornado.gen
 
 from odin.adapters.proxy import ProxyTarget, ProxyAdapter
+from odin.testing.utils import LogCaptureFilter
 
 class ProxyTestHandler(RequestHandler):
 
@@ -24,24 +25,54 @@ class ProxyTestHandler(RequestHandler):
     def get(self):
         self.write(ProxyTestHandler.data)
 
+class ProxyTestServer(object):
+
+    def __init__(self):
+
+        self.server_ioloop = IOLoop()
+
+        @tornado.gen.coroutine
+        def init_server():
+            sock, self.port = bind_unused_port()
+            app = Application([('/', ProxyTestHandler)])
+            self.server = HTTPServer(app)
+            self.server.add_socket(sock)
+        self.server_ioloop.run_sync(init_server)
+
+        self.server_thread = threading.Thread(target=self.server_ioloop.start)
+
+    def start(self):
+
+        self.server_thread.start()
+
+    def stop(self):
+
+        def stop_server():
+
+            self.server.stop()
+
+            @tornado.gen.coroutine
+            def slow_stop():
+                for i in range(5):
+                    yield
+                self.server_ioloop.stop()
+            
+            self.server_ioloop.add_callback(slow_stop)
+
+        self.server_ioloop.add_callback(stop_server)
+        self.server_thread.join()
+        self.server_ioloop.close(all_fds=True)
+
+
 class TestProxyTarget():
 
     @classmethod
     def setup_class(cls):
 
-        cls.server_ioloop = IOLoop()
+        cls.test_server = ProxyTestServer()
+        cls.test_server.start()
+        cls.port = cls.test_server.port
 
-        @tornado.gen.coroutine
-        def init_server():
-            sock, cls.port = bind_unused_port()
-            app = Application([('/', ProxyTestHandler)])
-            cls.server = HTTPServer(app)
-            cls.server.add_socket(sock)
-        cls.server_ioloop.run_sync(init_server)
-
-        cls.server_thread = threading.Thread(target=cls.server_ioloop.start)
-        cls.server_thread.start()
-       
         cls.name = 'test_target'
         cls.url = 'http://127.0.0.1:{}/'.format(cls.port)
         cls.request_timeout = 0.1
@@ -52,27 +83,7 @@ class TestProxyTarget():
     def teardown_class(cls):
 
         cls.proxy_target.http_client.close()
-
-        def stop_server():
-
-            cls.server.stop()
-            # Delay the shutdown of the IOLoop by several iterations because
-            # the server may still have some cleanup work left when
-            # the client finishes with the response (this is noticeable
-            # with http/2, which leaves a Future with an unexamined
-            # StreamClosedError on the loop).
-
-            @tornado.gen.coroutine
-            def slow_stop():
-                # The number of iterations is difficult to predict. Typically,
-                # one is sufficient, although sometimes it needs more.
-                for i in range(5):
-                    yield
-                cls.server_ioloop.stop()
-            cls.server_ioloop.add_callback(slow_stop)
-        cls.server_ioloop.add_callback(stop_server)
-        cls.server_thread.join()
-        cls.server_ioloop.close(all_fds=True)
+        cls.test_server.stop()
 
     def test_proxy_target_init(self):
 
@@ -85,7 +96,7 @@ class TestProxyTarget():
         self.proxy_target.last_update = ''
 
         self.proxy_target._update()
-        assert_equal(self.proxy_target.data, TestHandler.data)
+        assert_equal(self.proxy_target.data, ProxyTestHandler.data)
         assert_equal(self.proxy_target.status_code, 200)
         assert_not_equal(self.proxy_target.last_update, '')
 
@@ -119,12 +130,32 @@ class TestProxyAdapter():
     @classmethod
     def setup_class(cls):
 
+        cls.log_capture_filter = LogCaptureFilter()
+        logging.getLogger().setLevel(logging.DEBUG)
+
+        cls.num_targets = 2
+
+        cls.test_servers = []
+        cls.ports = []
+        cls.target_config = ""
+
+        for _ in range(cls.num_targets):
+
+            test_server = ProxyTestServer()
+            test_server.start()
+            cls.test_servers.append(test_server)
+            cls.ports.append(test_server.port)
+
+        cls.target_config = ','.join([
+            "node_{}=http://127.0.0.1:{}/".format(tgt, port) for (tgt, port) in enumerate(cls.ports)
+        ])
+
         cls.adapter_kwargs = {
-            'targets': "node_1=http://127.0.0.1:8888/api/0.1/system_info/,"+
-            "node_2=http://127.0.0.1:8887/api/0.1/system_info/",
-            'request_timeout': 2.0,
+            'targets': cls.target_config,
+            'request_timeout': 1.0,
         }
         cls.adapter = ProxyAdapter(**cls.adapter_kwargs)
+
         cls.path = ''
         cls.request = Mock
         cls.request.headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
@@ -134,8 +165,52 @@ class TestProxyAdapter():
 
         for target in cls.adapter.targets:
             target.http_client.close()
+        for test_server in cls.test_servers:
+            test_server.stop()
+
+    def test_adapter_loaded(self):
+
+        expected_msg = "ProxyAdapter with {} targets loaded".format(self.num_targets)
+        assert_true(expected_msg in self.log_capture_filter.log_debug())
 
     def test_adapter_get(self):
 
         response = self.adapter.get(self.path, self.request)
-        print(response.data)
+        
+        assert_equal(len(response.data), self.num_targets)
+        
+        for tgt in range(self.num_targets):
+            node_str = 'node_{}'.format(tgt)
+            assert_true(node_str in response.data)
+            assert_equal(response.data[node_str]['data'], ProxyTestHandler.data)
+
+    def test_adapter_get_bad_path(self):
+
+        missing_path = 'missing/path'
+        response = self.adapter.get(missing_path, self.request)
+
+        assert_true('error' in response.data)
+        assert_equal('The path {} is invalid'.format(missing_path), response.data['error'])
+
+    def test_adapter_bad_timeout(self):
+
+        bad_timeout = 'not_timeout'
+        bad_adapter = ProxyAdapter(request_timeout=bad_timeout)
+
+        expected_msg = 'Illegal timeout specified for ProxyAdapter: {}'.format(bad_timeout)
+        assert_true(expected_msg in self.log_capture_filter.log_error())
+
+    def test_adapter_bad_target_spec(self):
+
+        bad_target_spec = 'bad_target_1,bad_target_2'
+        bad_adapter = ProxyAdapter(targets=bad_target_spec)
+
+        expected_msg = 'Illegal target specification for ProxyAdapter: bad_target_1'
+        assert_true(expected_msg in self.log_capture_filter.log_error())
+
+    def test_adapter_no_target_spec(self):
+
+        bad_adapter = ProxyAdapter()
+
+        expected_msg = "Failed to resolve targets for ProxyAdapter"
+        assert_true(expected_msg in self.log_capture_filter.log_error())
