aa371fae4ad7aa6c8b31cd4c23f3ebed08f6e43d
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2019-01-28 14:01:42 +0000

    Update SystemStatus unit tests to reflect OS-indepdence changes

--- odin/testing/adapters/test_system_status.py
+++ odin/testing/adapters/test_system_status.py
@@ -1,13 +1,14 @@
 import sys
-
+import platform
 from nose.tools import *
+import psutil
 
 if sys.version_info[0] == 3:  # pragma: no cover
-    from unittest.mock import Mock
+    from unittest.mock import Mock, patch
 else:                         # pragma: no cover
-    from mock import Mock
+    from mock import Mock, patch
 
-from odin.adapters.system_status import SystemStatusAdapter, SystemStatus
+from odin.adapters.system_status import SystemStatusAdapter, SystemStatus, Singleton
 from odin.adapters.parameter_tree import ParameterTreeError
 
 class TestSystemStatus():
@@ -15,7 +16,18 @@ class TestSystemStatus():
     @classmethod
     def setup_class(cls):
 
-        cls.system_status = SystemStatus(interfaces="lo, bad", disks="/, /bad", processes='python, proc2', rate=0.001)
+        if platform.system() == 'Darwin':
+            cls.lo_iface='lo0'
+        else:
+            cls.lo_iface='lo'
+            
+        cls.interfaces="{}, bad".format(cls.lo_iface)
+        cls.disks = "/, /bad"
+        cls.processes = "python, proc2"
+        cls.rate = 0.001
+
+        cls.system_status = SystemStatus(
+            interfaces=cls.interfaces, disks=cls.disks, processes=cls.processes, rate=cls.rate)
 
     def test_system_status_single_instance(self):
         new_instance = SystemStatus()
@@ -28,8 +40,8 @@ class TestSystemStatus():
         result = self.system_status.get('')
         assert_equal(type(result), dict)
 
-    def test_system_status_add_process(self):
-        self.system_status.add_process('proc1')
+    def test_system_status_add_processes(self):
+        self.system_status.add_processes('proc1')
         assert_true('proc1' in self.system_status._processes)
 
     def test_system_status_check_bad_nic(self):
@@ -38,7 +50,7 @@ class TestSystemStatus():
 
     def test_system_status_monitor_network(self):
         self.system_status.monitor_network()
-        result = self.system_status.get('status/network/lo')
+        result = self.system_status.get('status/network/{}'.format(self.lo_iface))
         assert_equal(type(result), dict)
 
     def test_system_status_monitor_disks(self):
@@ -65,16 +77,16 @@ class TestSystemStatus():
         self.system_status.monitor_network()
 
     def test_bad_process_exception(self):
-        self.system_status._processes["rubbish"] = {}
+        self.system_status._processes["rubbish"] = []
         # Any exceptions caught whilst monitoring will be handled within the class
         self.system_status.monitor_processes()
 
     def test_add_process_exception(self):
-        self.stash_method = self.system_status.find_process
-        self.system_status.find_process = Mock(side_effect=KeyError('error'))
+        self.stash_method = self.system_status.find_processes
+        self.system_status.find_processes = Mock(side_effect=KeyError('error'))
         # Any exceptions caught whilst adding processes will be handled within the class
-        self.system_status.add_process("process")
-        self.system_status.find_process = self.stash_method
+        self.system_status.add_processes("process")
+        self.system_status.find_processes = self.stash_method
 
     def test_update_loop_exception(self):
         self.stash_method = self.system_status.monitor
@@ -83,6 +95,65 @@ class TestSystemStatus():
         self.system_status.update_loop()
         self.system_status.monitor = self.stash_method
 
+    def test_default_rate_argument(self):
+
+        stash_singleton = dict(Singleton._instances)
+        Singleton._instances = {}
+        temp_system_status = SystemStatus(
+            interfaces=self.interfaces, disks=self.disks, processes=self.processes,
+        )
+        assert_almost_equal(1.0, temp_system_status._update_interval)
+        Singleton._instances = {}
+        Singleton._instances = dict(stash_singleton)
+
+    def test_num_processes_change(self):
+
+        self.stash_method = self.system_status.find_processes
+        self.stash_processes = dict(self.system_status._processes)
+        self.system_status._processes = {}
+        self.system_status._processes['python'] = self.stash_processes['python']
+        current_processes = self.system_status.find_processes('python')
+        patched_processes = list(current_processes)
+        patched_processes.append(current_processes[0])
+
+        self.system_status.find_processes = Mock(return_value = patched_processes)
+        self.system_status.monitor_processes()
+        # monitor_process will detect change in number of processes
+        self.system_status.find_processes = self.stash_method
+        self.system_status._processes = self.stash_processes
+
+    def test_monitor_process_cpu_affinity(self):
+
+        self.stash_proc = self.system_status._processes['python'][0]
+
+        setattr(self.system_status._processes['python'][0], 'cpu_affinity', lambda: [1,2,3])
+        self.system_status.monitor_processes()
+
+        delattr(self.system_status._processes['python'][0], 'cpu_affinity')
+        self.system_status.monitor_processes()
+
+        self.system_status._processes['python'][0] = self.stash_proc
+
+    def test_monitor_process_traps_nosuchprocess(self):
+
+        with patch('psutil.Process.memory_info', spec=True) as mocked:
+            mocked.side_effect = psutil.NoSuchProcess('')
+            self.system_status.monitor_processes()
+
+    def test_monitor_process_traps_accessdenied(self):
+
+        with patch('psutil.Process.memory_info', spec=True) as mocked:
+            mocked.side_effect = psutil.AccessDenied('')
+            self.system_status.monitor_processes()
+        
+    def test_find_processes_traps_accessdenied(self):
+
+        with patch('psutil.Process.cpu_percent', spec=True) as mocked:
+            mocked.side_effect = psutil.AccessDenied('')
+            processes = self.system_status.find_processes('python') 
+            # If all processes are AccessDenied then the returned list will be empty
+            assert_false(processes)
+        
 
 class TestSystemStatusAdapter():
 
