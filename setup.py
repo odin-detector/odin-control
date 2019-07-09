9e6c6c4c33525a16ca8207277d389da6901c7bdc
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2019-07-09 16:58:22 +0100

    Migrate package into src directory.
    In preparation for updating the package installation, dependency management
    and testing for using tox to test across a range of python and tornado versions,
    the odin package has been moved into a src/ directory. Alongside the recent
    change to put unit testing modules into their own test directory, this brings
    the package more into line with best recommendations for file structure.
    
    The setup.py script has been modified to reflect the new src structure and also
    to no longer use requirements.txt to pin immediate and transitive package
    dependencies. Instead setup.py defines minimum useable versions for immediate
    dependencies, also in line with accepted best practice for package management.
    
    Versioneer support for the new structure has also been updated to the latest
    version and appropriate manifest declaration

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@ import sys
 from setuptools import setup, find_packages
 import versioneer
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+# with open('requirements.txt') as f:
+#     required = f.read().splitlines()
 
-if sys.version_info[0] == 2:
-    required.append('futures>=3.0.0')
+# if sys.version_info[0] == 2:
+#     required.append('futures>=3.0.0')
+
+install_requires = [
+    'tornado>=4.3',
+    'pyzmq>=15.2.0',
+    'future',
+    'psutil>=5.0',
+]
+
+#if sys.version_info[0] < 3:
+#    install_requires.append('futures>=3.0.0')
 
 setup(
     name="odin",
@@ -16,11 +26,12 @@ setup(
     url='https://github.com/timcnicholls/odin',
     author='Tim Nicholls',
     author_email='tim.nicholls@stfc.ac.uk',
-    packages=find_packages(),
+    packages=find_packages('src'),
+    package_dir={'':'src'},
     entry_points={
         'console_scripts' : [
             'odin_server = odin.server:main',
         ],
     },
-    install_requires=required,
+    install_requires=install_requires,
 )
