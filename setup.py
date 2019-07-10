3c87b478d5ca05ca1a18364e3d14909438c117c2
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2019-07-10 08:08:29 +0100

    Remove commented out old code from setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,12 @@ import sys
 from setuptools import setup, find_packages
 import versioneer
 
-# with open('requirements.txt') as f:
-#     required = f.read().splitlines()
-
-# if sys.version_info[0] == 2:
-#     required.append('futures>=3.0.0')
-
 install_requires = [
     'tornado>=4.3',
     'pyzmq>=15.2.0',
     'future',
     'psutil>=5.0',
 ]
-
-#if sys.version_info[0] < 3:
-#    install_requires.append('futures>=3.0.0')
 
 setup(
     name="odin",
