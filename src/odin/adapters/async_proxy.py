81d2e426e3fb40eb2a80d1249ffd29b246d97f4b
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2022-04-06 16:17:56 +0100

    Implement async proxy adapter and refactor existing proxy.
    The existing proxy adapter is refactored to create base_proxy.py, containing
    base classes relevant for both sync and async proxies.

--- src/odin/adapters/async_proxy.py
+++ src/odin/adapters/async_proxy.py
@@ -1,80 +1,96 @@
 """
-Proxy adapter for use in odin-control.
+Asynchronous proxy adapter for use in odin-control.
 
-This module implements a simple proxy adapter, allowing requests to be proxied to
+This module implements a simple asynchronous proxy adapter, allowing requests to be proxied to
 one or more remote HTTP resources, typically further odin-control instances.
 
 Tim Nicholls, Ashley Neaves STFC Detector Systems Software Group.
 """
-from tornado.httpclient import HTTPClient
+import asyncio
+import inspect
+
+from tornado.httpclient import AsyncHTTPClient
 from odin.util import decode_request_body
 from odin.adapters.adapter import (
-    ApiAdapter, ApiAdapterResponse,
-    request_types, response_types, wants_metadata
+    ApiAdapterResponse, request_types, response_types, wants_metadata
 )
+from odin.adapters.async_adapter import AsyncApiAdapter
 from odin.adapters.base_proxy import BaseProxyTarget, BaseProxyAdapter
 
 
-class ProxyTarget(BaseProxyTarget):
+class AsyncProxyTarget(BaseProxyTarget):
     """
-    Proxy adapter target class.
+    Asynchronous proxy adapter target class.
 
-    This class implements a proxy target, its parameter tree and associated
+    This class implements an asynchronous proxy target, its parameter tree and associated
     status information for use in the ProxyAdapter.
     """
 
     def __init__(self, name, url, request_timeout):
         """
-        Initialise the ProxyTarget object.
+        Initialise the AsyncProxyTarget object.
 
-        This constructor initialises the ProxyTarget, creating a HTTP client and delegating
-        the full initialisation to the base class.
+        This constructor initialises the AsyncProxyTarget, creating an async HTTP client and
+        delegating the full initialisation to the base class.
+
 
         :param name: name of the proxy target
         :param url: URL of the remote target
         :param request_timeout: request timeout in seconds
         """
         # Create an async HTTP client for use in this target
-        self.http_client = HTTPClient()
+        self.http_client = AsyncHTTPClient()
 
         # Initialise the base class
-        super(ProxyTarget, self).__init__(name, url, request_timeout)
+        super(AsyncProxyTarget, self).__init__(name, url, request_timeout)
 
-        # Initialise the data and metadata trees from the remote target
-        self.remote_get()
-        self.remote_get(get_metadata=True)
+    def __await__(self):
+        """
+        Make AsyncProxyTarget objects awaitable.
 
-    def remote_get(self, path='', get_metadata=False):
+        This magic method makes the instantation of AsyncProxyTarget objects awaitable. This allows
+        the async calls to remote_get, used to populate the data and metadata trees from the remote
+        target, to be awaited.
+        """
+        async def closure():
+            """Await the calls to the remote target to populate and data and metadata tress."""
+            await self.remote_get()
+            await self.remote_get(get_metadata=True)
+            return self
+
+        return closure().__await__()
+
+    async def remote_get(self, path='', get_metadata=False):
         """
         Get data from the remote target.
 
-        This method updates the local proxy target with new data by issuing a GET request to the
+        This async method requests data from the remote target by issuing a GET request to the
         target URL, and then updates the local proxy target data and status information according to
         the response. The detailed handling of this is implemented by the base class.
 
         :param path: path to data on remote target
         :param get_metadata: flag indicating if metadata is to be requested
         """
-        super(ProxyTarget, self).remote_get(path, get_metadata)
+        await super(AsyncProxyTarget, self).remote_get(path, get_metadata)
 
-    def remote_set(self, path, data):
+    async def remote_set(self, path, data):
         """
         Set data on the remote target.
 
-        This method sends data to the remote target by issuing a PUT request to the target
+        This async method sends data to the remote target by issuing a PUT request to the target
         URL, and then updates the local proxy target data and status information according to the
         response. The detailed handling of this is implemented by the base class.
 
         :param path: path to data on remote target
         :param data: data to set on remote target
         """
-        super(ProxyTarget, self).remote_set(path, data)
+        await super(AsyncProxyTarget, self).remote_set(path, data)
 
-    def _send_request(self, request, path, get_metadata=False):
+    async def _send_request(self, request, path, get_metadata=False):
         """
         Send a request to the remote target and update data.
 
-        This internal method sends a request to the remote target using the HTTP client
+        This internal async method sends a request to the remote target using the HTTP client
         and handles the response, updating target data accordingly.
 
         :param request: HTTP request to transmit to target
@@ -83,7 +99,7 @@ class ProxyTarget(BaseProxyTarget):
         """
         # Send the request to the remote target, handling any exceptions that occur
         try:
-            response = self.http_client.fetch(request)
+            response = await self.http_client.fetch(request)
         except Exception as fetch_exception:
             # Set the response to the exception so it can be handled during response resolution
             response = fetch_exception
@@ -92,9 +108,9 @@ class ProxyTarget(BaseProxyTarget):
         self._process_response(response, path, get_metadata)
 
 
-class ProxyAdapter(ApiAdapter, BaseProxyAdapter):
+class AsyncProxyAdapter(AsyncApiAdapter, BaseProxyAdapter):
     """
-    Proxy adapter class for odin-control.
+    Asynchronous proxy adapter class for odin-control.
 
     This class implements a proxy adapter, allowing odin-control to forward requests to
     other HTTP services.
@@ -102,7 +118,7 @@ class ProxyAdapter(ApiAdapter, BaseProxyAdapter):
 
     def __init__(self, **kwargs):
         """
-        Initialise the ProxyAdapter.
+        Initialise the AsyncProxyAdapter.
 
         This constructor initialises the adapter instance. The base class adapter is initialised
         with the keyword arguments and then the proxy targets and paramter tree initialised by the
@@ -111,17 +127,34 @@ class ProxyAdapter(ApiAdapter, BaseProxyAdapter):
         :param kwargs: keyword arguments specifying options
         """
         # Initialise the base class
-        super(ProxyAdapter, self).__init__(**kwargs)
+        super(AsyncProxyAdapter, self).__init__(**kwargs)
 
         # Initialise the proxy targets and parameter trees
-        self.initialise_proxy(ProxyTarget)
+        self.initialise_proxy(AsyncProxyTarget)
+
+    def __await__(self):
+        """
+        Make AsyncProxyAdapter objects awaitable.
+
+        This magic method makes the instantation of AsyncProxyAdapter objects awaitable. This allows
+        the async proxy targets to be awaited at initialisation.
+        """
+
+        async def closure():
+            """Construct a list of awaitable attributes and targets and await initialisation."""
+            awaitables = [attr for attr in self.__dict__.values() if inspect.isawaitable(attr)]
+            awaitables += [target for target in self.targets if inspect.isawaitable(target)]
+            await asyncio.gather(*awaitables)
+            return self
+
+        return closure().__await__()
 
     @response_types('application/json', default='application/json')
-    def get(self, path, request):
+    async def get(self, path, request):
         """
         Handle an HTTP GET request.
 
-        This method handles an HTTP GET request, returning a JSON response. The request is
+        This async method handles an HTTP GET request, returning a JSON response. The request is
         passed to the adapter proxy and resolved into responses from the requested proxy targets.
 
         :param path: URI path of request
@@ -130,18 +163,18 @@ class ProxyAdapter(ApiAdapter, BaseProxyAdapter):
         """
         get_metadata = wants_metadata(request)
 
-        self.proxy_get(path, get_metadata)
+        await asyncio.gather(*self.proxy_get(path, get_metadata))
         (response, status_code) = self._resolve_response(path, get_metadata)
 
         return ApiAdapterResponse(response, status_code=status_code)
 
     @request_types("application/json", "application/vnd.odin-native")
     @response_types('application/json', default='application/json')
-    def put(self, path, request):
+    async def put(self, path, request):
         """
         Handle an HTTP PUT request.
 
-        This method handles an HTTP PUT request, returning a JSON response. The request is
+        This async method handles an HTTP PUT request, returning a JSON response. The request is
         passed to the adapter proxy to set data on the remote targets and resolved into responses
         from those targets.
 
@@ -157,7 +190,7 @@ class ProxyAdapter(ApiAdapter, BaseProxyAdapter):
             response = {'error': 'Failed to decode PUT request body: {}'.format(str(type_val_err))}
             status_code = 415
         else:
-            self.proxy_set(path, body)
+            await asyncio.gather(*self.proxy_set(path, body))
             (response, status_code) = self._resolve_response(path)
 
         return ApiAdapterResponse(response, status_code=status_code)
