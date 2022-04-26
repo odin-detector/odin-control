e01c1e357bf71fa80a55f23f73ef6f85bdbfe3fb
Gary Yendell <gary.yendell@diamond.ac.uk>
2022-04-26 13:08:43 +0100

    Update packaging

--- setup.py
+++ setup.py
@@ -1,42 +1,10 @@
 import sys
-from setuptools import setup, find_packages
+
+from setuptools import setup
+
 import versioneer
 
-install_requires = [
-    'tornado>=4.3',
-    'pyzmq>=17.0',
-    'future',
-    'psutil>=5.0',
-]
-
-extras_require = {
-    'test': [
-        'pytest', 'pytest-cov', 'requests', 'tox'
-    ]
-}
-
-if sys.version_info[0] == 2:
-    install_requires.append('futures')
-    extras_require['test'].append('mock')
-else:
-    extras_require['test'].append('pytest-asyncio')
-
 setup(
-    name="odin_control",
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description='ODIN detector control system',
-    url='https://github.com/odin-detector/odin-control',
-    author='Tim Nicholls',
-    author_email='tim.nicholls@stfc.ac.uk',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    entry_points={
-        'console_scripts': [
-            'odin_server = odin.server:main_deprecate',
-            'odin_control = odin.server:main',
-        ],
-    },
-    install_requires=install_requires,
-    extras_require=extras_require,
+    cmdclass=versioneer.get_cmdclass()
 )
